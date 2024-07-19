21f0fb1c6a018ba8c4e688e34ef4dc9821f87d40
chennakesava13 <kchenna039@gmail.com>
2024-07-19 10:23:56 +0000

    roshan

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <p>This is a simple React project.</p>
+    </div>
+  );
+}
+
+export default App;
